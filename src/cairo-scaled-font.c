saitoha/cairo

commit 676b221326cacdd205c225bba8ac43378b4abb81

Protect against NULL glyphs array in text API
We still don't do anything if num_glyphs is negative.  Why isn't
that argument unsigned int...

src/cairo-scaled-font.c
@@ -1176,6 +1176,15 @@ cairo_scaled_font_glyph_extents (cairo_scaled_font_t   *scaled_font,
 	return;
     }
 
+    if (num_glyphs == 0)
+	return;
+
+    if (glyphs == NULL) {
+	cairo_status_t status = _cairo_error (CAIRO_STATUS_NULL_POINTER);
+	/* XXX Can't propagate error */
+	return;
+    }
+
     CAIRO_MUTEX_LOCK (scaled_font->mutex);
     _cairo_scaled_font_freeze_cache (scaled_font);
 

src/cairo.c
@@ -2949,7 +2949,7 @@ cairo_get_scaled_font (cairo_t *cr)
 /**
  * cairo_text_extents:
  * @cr: a #cairo_t
- * @utf8: a string of text, encoded in UTF-8
+ * @utf8: a string of text encoded in UTF-8, or %NULL
  * @extents: a #cairo_text_extents_t object into which the results
  * will be stored
  *
@@ -3042,6 +3042,14 @@ cairo_glyph_extents (cairo_t                *cr,
     if (cr->status)
 	return;
 
+    if (num_glyphs == 0)
+	return;
+
+    if (glyphs == NULL) {
+	_cairo_set_error (cr, CAIRO_STATUS_NULL_POINTER);
+	return;
+    }
+
     status = _cairo_gstate_glyph_extents (cr->gstate, glyphs, num_glyphs,
 					  extents);
     if (status)
@@ -3051,7 +3059,7 @@ cairo_glyph_extents (cairo_t                *cr,
 /**
  * cairo_show_text:
  * @cr: a cairo context
- * @utf8: a string of text encoded in UTF-8
+ * @utf8: a string of text encoded in UTF-8, or %NULL
  *
  * A drawing operator that generates the shape from a string of UTF-8
  * characters, rendered according to the current font_face, font_size
@@ -3145,6 +3153,11 @@ cairo_show_glyphs (cairo_t *cr, const cairo_glyph_t *glyphs, int num_glyphs)
     if (num_glyphs == 0)
 	return;
 
+    if (glyphs == NULL) {
+	_cairo_set_error (cr, CAIRO_STATUS_NULL_POINTER);
+	return;
+    }
+
     status = _cairo_gstate_show_glyphs (cr->gstate, glyphs, num_glyphs);
     if (status)
 	_cairo_set_error (cr, status);
@@ -3153,7 +3166,7 @@ cairo_show_glyphs (cairo_t *cr, const cairo_glyph_t *glyphs, int num_glyphs)
 /**
  * cairo_text_path:
  * @cr: a cairo context
- * @utf8: a string of text encoded in UTF-8
+ * @utf8: a string of text encoded in UTF-8, or %NULL
  *
  * Adds closed paths for text to the current path.  The generated
  * path if filled, achieves an effect similar to that of
@@ -3248,6 +3261,11 @@ cairo_glyph_path (cairo_t *cr, const cairo_glyph_t *glyphs, int num_glyphs)
 
     if (num_glyphs == 0)
 	return;
+
+    if (glyphs == NULL) {
+	_cairo_set_error (cr, CAIRO_STATUS_NULL_POINTER);
+	return;
+    }
 
     status = _cairo_gstate_glyph_path (cr->gstate,
 				       glyphs, num_glyphs,

commit dd4ec1ce3ef4593264c93fe71c288b83ce6ec0b8

Make cairo_scaled_font_glyph_extents() unconditionally zero the extents
This makes cairo_scaled_font_glyph_extents() match a similar pattern of
initialization in cairo_glyph_extents(). It also fixes the 'text-zero-len' test
that was broken by 676b221326cacdd205c225bba8ac43378b4abb81.

src/cairo-scaled-font.c
@@ -1166,15 +1166,15 @@ cairo_scaled_font_glyph_extents (cairo_scaled_font_t   *scaled_font,
     cairo_bool_t visible = FALSE;
     cairo_scaled_glyph_t *scaled_glyph = NULL;
 
-    if (scaled_font->status) {
-	extents->x_bearing = 0.0;
-	extents->y_bearing = 0.0;
-	extents->width  = 0.0;
-	extents->height = 0.0;
-	extents->x_advance = 0.0;
-	extents->y_advance = 0.0;
+    extents->x_bearing = 0.0;
+    extents->y_bearing = 0.0;
+    extents->width  = 0.0;
+    extents->height = 0.0;
+    extents->x_advance = 0.0;
+    extents->y_advance = 0.0;
+
+    if (scaled_font->status)
 	return;
-    }
 
     if (num_glyphs == 0)
 	return;