saitoha/cairo

commit 1a9809baab5d8adf6ccf165e8f5d294b498c2fd5

Fix scaling with a device offset
Apply the device offset transformation to the pattern before the ctm one.
Fixes bug #16469.

src/cairo-gstate.c
@@ -795,14 +795,15 @@ _cairo_gstate_copy_transformed_pattern (cairo_gstate_t  *gstate,
     if (status)
 	return status;
 
-    _cairo_pattern_transform (pattern, ctm_inverse);
-
+    /* apply device_transform first so that it is transformed by ctm_inverse */
     if (cairo_pattern_get_type (original) == CAIRO_PATTERN_TYPE_SURFACE) {
         surface_pattern = (cairo_surface_pattern_t *) original;
         surface = surface_pattern->surface;
         if (_cairo_surface_has_device_transform (surface))
             _cairo_pattern_transform (pattern, &surface->device_transform);
     }
+
+    _cairo_pattern_transform (pattern, ctm_inverse);
 
     return CAIRO_STATUS_SUCCESS;
 }

commit 26c2dbe1fab4f484dee0a65baa7efcd9bbc457d1

Add a test case showing the invariance of push/pop_group
This shows the regressions caused by
1a9809baab5d8adf6ccf165e8f5d294b498c2fd5

test/Makefile.am
@@ -21,6 +21,7 @@ clip-operator$(EXEEXT)					\
 clip-push-group$(EXEEXT)				\
 clip-twice$(EXEEXT)					\
 clip-zero$(EXEEXT)					\
+clipped-group$(EXEEXT)					\
 close-path$(EXEEXT)					\
 composite-integer-translate-source$(EXEEXT)		\
 composite-integer-translate-over$(EXEEXT)		\
@@ -325,6 +326,8 @@ REFERENCE_IMAGES = \
 	clip-twice-quartz-rgb24-ref.png	\
 	clip-twice-ref.png	\
 	clip-twice-rgb24-ref.png	\
+	clipped-group-ref.png	\
+	clipped-group-ps-ref.png	\
 	close-path-ref.png	\
 	close-path-ps-ref.png	\
 	composite-integer-translate-over-ref.png	\

test/clipped-group.c
@@ -1,0 +1,96 @@
+/*
+ * Copyright © 2008 Mozilla Corporation
+ *
+ * Permission to use, copy, modify, distribute, and sell this software
+ * and its documentation for any purpose is hereby granted without
+ * fee, provided that the above copyright notice appear in all copies
+ * and that both that copyright notice and this permission notice
+ * appear in supporting documentation, and that the name of
+ * Mozilla Corporation not be used in advertising or publicity pertaining to
+ * distribution of the software without specific, written prior
+ * permission. Mozilla Corporation makes no representations about the
+ * suitability of this software for any purpose.  It is provided "as
+ * is" without express or implied warranty.
+ *
+ * MOZILLA CORPORATION DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS
+ * SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS, IN NO EVENT SHALL MOZILLA CORPORATION BE LIABLE FOR ANY SPECIAL,
+ * INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+ * OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+ * IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * Author: Jeff Muizelaar
+ */
+
+#include "cairo-test.h"
+
+#define WIDTH 60
+#define HEIGHT 70
+
+static cairo_test_draw_function_t draw;
+
+cairo_test_t test = {
+    "clipped-group",
+    "Test that a clipped group ends up in the right place",
+    WIDTH, HEIGHT,
+    draw
+};
+
+static cairo_test_status_t
+draw (cairo_t *cr, int width, int height)
+{
+    /* fill with black so we don't need an rgb test case */
+    cairo_set_source_rgb (cr, 0, 0, 0);
+    cairo_paint (cr);
+
+    /* setting a scale will ensure that the device offset is transformed */
+    cairo_scale (cr, 2.1, 2.8);
+    cairo_set_source_rgb (cr, 1, .5,.4);
+
+    /* all rectangles should look the same */
+
+    /* plain rectangle */
+    cairo_rectangle (cr, 4, 4, 8, 8);
+    cairo_fill (cr);
+
+    cairo_translate (cr, 10, 0);
+
+    /* clipped rectangle */
+    cairo_save (cr);
+    cairo_rectangle (cr, 3, 3, 9, 9);
+    cairo_clip (cr);
+    cairo_rectangle (cr, 4, 4, 8, 8);
+    cairo_fill (cr);
+    cairo_restore (cr);
+
+    cairo_translate (cr, 0, 10);
+
+    /* clipped and grouped rectangle */
+    cairo_save (cr);
+    cairo_rectangle (cr, 3, 3, 9, 9);
+    cairo_clip (cr);
+    cairo_push_group (cr);
+    cairo_rectangle (cr, 4, 4, 8, 8);
+    cairo_fill (cr);
+    cairo_pop_group_to_source (cr);
+    cairo_paint (cr);
+    cairo_restore (cr);
+
+    cairo_translate (cr, -10, 0);
+
+    /* grouped rectangle */
+    cairo_push_group (cr);
+    cairo_rectangle (cr, 4, 4, 8, 8);
+    cairo_fill (cr);
+    cairo_pop_group_to_source (cr);
+    cairo_paint (cr);
+
+    return CAIRO_TEST_SUCCESS;
+}
+
+int
+main (void)
+{
+    return cairo_test (&test);
+}