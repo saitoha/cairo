saitoha/cairo

commit 3d3d10a31e04498ef91a288d89b3dec291bfc323

[skiplist] Missing include for ffs()

src/cairo-skiplist.c
@@ -25,6 +25,8 @@
 
 #include "cairo-skiplist-private.h"
 
+#include <strings.h> /* ffs() */
+
 #define ELT_DATA(elt) (void *)	((char*) (elt) - list->data_size)
 #define NEXT_TO_ELT(next)	(skip_elt_t *) ((char *) (next) - offsetof (skip_elt_t, next))
 

commit 56580a0e68abd2145795e0006db8e719ecdb2c8c

Only include <strings.h> for ffs() when we HAVE_FFS
This fixes breakage caused by 3d3d10a31e04498ef91a288d89b3dec291bfc323

src/cairo-skiplist.c
@@ -25,7 +25,9 @@
 
 #include "cairo-skiplist-private.h"
 
+#if HAVE_FFS
 #include <strings.h> /* ffs() */
+#endif
 
 #define ELT_DATA(elt) (void *)	((char*) (elt) - list->data_size)
 #define NEXT_TO_ELT(next)	(skip_elt_t *) ((char *) (next) - offsetof (skip_elt_t, next))