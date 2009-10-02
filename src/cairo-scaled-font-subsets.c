saitoha/cairo

commit 1ae5a41951b8a8cb415835559f91d1636a885216

PDF: Only Type 3 fonts should include glyph 0 in /ToUnicode
4c498098 made all 8-bit fonts include glyph 0 which incorrectly
included Type 1 fallback fonts.

src/cairo-scaled-font-subsets.c
@@ -570,6 +570,7 @@ _cairo_sub_font_collect (void *entry, void *closure)
 
 	subset.scaled_font = sub_font->scaled_font;
 	subset.is_composite = sub_font->is_composite;
+	subset.is_scaled = sub_font->is_scaled;
 	subset.font_id = sub_font->font_id;
 	subset.subset_id = i;
 	subset.glyphs = collection->glyphs;

src/cairo-pdf-surface.c
@@ -3673,31 +3673,8 @@ _cairo_pdf_surface_emit_to_unicode_stream (cairo_pdf_surface_t		*surface,
     _cairo_output_stream_printf (surface->output,
                                   "endcodespacerange\n");
 
-    if (is_composite) {
-	num_bfchar = font_subset->num_glyphs - 1;
-
-	/* The CMap specification has a limit of 100 characters per beginbfchar operator */
-	_cairo_output_stream_printf (surface->output,
-				     "%d beginbfchar\n",
-				     num_bfchar > 100 ? 100 : num_bfchar);
-
-	for (i = 0; i < num_bfchar; i++) {
-	    if (i != 0 && i % 100 == 0) {
-		_cairo_output_stream_printf (surface->output,
-					     "endbfchar\n"
-					     "%d beginbfchar\n",
-					     num_bfchar - i > 100 ? 100 : num_bfchar - i);
-	    }
-	    _cairo_output_stream_printf (surface->output, "<%04x> ", i + 1);
-	    status = _cairo_pdf_surface_emit_unicode_for_glyph (surface,
-								font_subset->utf8[i + 1]);
-	    if (unlikely (status))
-		return status;
-
-	    _cairo_output_stream_printf (surface->output,
-					 "\n");
-	}
-    } else {
+    if (font_subset->is_scaled) {
+	/* Type 3 fonts include glyph 0 in the subset */
 	num_bfchar = font_subset->num_glyphs;
 
 	/* The CMap specification has a limit of 100 characters per beginbfchar operator */
@@ -3715,6 +3692,35 @@ _cairo_pdf_surface_emit_to_unicode_stream (cairo_pdf_surface_t		*surface,
 	    _cairo_output_stream_printf (surface->output, "<%02x> ", i);
 	    status = _cairo_pdf_surface_emit_unicode_for_glyph (surface,
 								font_subset->utf8[i]);
+	    if (unlikely (status))
+		return status;
+
+	    _cairo_output_stream_printf (surface->output,
+					 "\n");
+	}
+    } else {
+	/* Other fonts reserve glyph 0 for .notdef. Omit glyph 0 from the /ToUnicode map */
+	num_bfchar = font_subset->num_glyphs - 1;
+
+	/* The CMap specification has a limit of 100 characters per beginbfchar operator */
+	_cairo_output_stream_printf (surface->output,
+				     "%d beginbfchar\n",
+				     num_bfchar > 100 ? 100 : num_bfchar);
+
+	for (i = 0; i < num_bfchar; i++) {
+	    if (i != 0 && i % 100 == 0) {
+		_cairo_output_stream_printf (surface->output,
+					     "endbfchar\n"
+					     "%d beginbfchar\n",
+					     num_bfchar - i > 100 ? 100 : num_bfchar - i);
+	    }
+	    if (is_composite)
+		_cairo_output_stream_printf (surface->output, "<%04x> ", i + 1);
+	    else
+		_cairo_output_stream_printf (surface->output, "<%02x> ", i + 1);
+
+	    status = _cairo_pdf_surface_emit_unicode_for_glyph (surface,
+								font_subset->utf8[i + 1]);
 	    if (unlikely (status))
 		return status;
 

src/cairoint.h
@@ -453,6 +453,7 @@ typedef struct _cairo_scaled_font_subset {
     char          **glyph_names;
     unsigned int num_glyphs;
     cairo_bool_t is_composite;
+    cairo_bool_t is_scaled;
 } cairo_scaled_font_subset_t;
 
 struct _cairo_scaled_font_backend {

commit 27ef2a5b02dcabcf602223bbad77d2a77e98e48d

PDF: Fix regression caused by 1ae5a419
Bug 24240 first char missing when printing a PDF.

src/cairo-scaled-font-subsets.c
@@ -294,7 +294,7 @@ _cairo_sub_font_create (cairo_scaled_font_subsets_t	*parent,
 
     /* Reserve first glyph in subset for the .notdef glyph except for
      * Type 3 fonts */
-    if (! scaled_font) {
+    if (! is_scaled) {
 	status = _cairo_sub_font_map_glyph (sub_font, 0, NULL, -1, &subset_glyph);
 	if (unlikely (status)) {
 	    _cairo_hash_table_destroy (sub_font->sub_font_glyphs);