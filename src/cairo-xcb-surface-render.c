saitoha/cairo

commit 6f4318869cf551f4ed349459b7afd50e4b10941c

xcb: Prefer RenderFillRectangles to perform the deferred clear
Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

src/cairo-xcb-surface-render.c
@@ -2282,17 +2282,27 @@ _cairo_xcb_surface_clear (cairo_xcb_surface_t *dst)
     if (unlikely (status))
 	return status;
 
-    gc = _cairo_xcb_screen_get_gc (dst->screen, dst->drawable, dst->depth);
-
     rect.x = rect.y = 0;
     rect.width  = dst->width;
     rect.height = dst->height;
 
-    _cairo_xcb_connection_poly_fill_rectangle (dst->connection,
-					       dst->drawable, gc,
-					       1, &rect);
+    if (dst->flags & CAIRO_XCB_RENDER_HAS_COMPOSITE) {
+	xcb_render_color_t transparent = { 0 };
 
-    _cairo_xcb_screen_put_gc (dst->screen, dst->depth, gc);
+	_cairo_xcb_connection_render_fill_rectangles (dst->connection,
+						      XCB_RENDER_PICT_OP_CLEAR,
+						      dst->picture,
+						      transparent,
+						      1, &rect);
+    } else {
+	gc = _cairo_xcb_screen_get_gc (dst->screen, dst->drawable, dst->depth);
+
+	_cairo_xcb_connection_poly_fill_rectangle (dst->connection,
+						   dst->drawable, gc,
+						   1, &rect);
+
+	_cairo_xcb_screen_put_gc (dst->screen, dst->depth, gc);
+    }
 
     _cairo_xcb_connection_release (dst->connection);
 

commit 09e512a9dbabc9667fa5ea6b16240993a4d562c7

xcb: Fix a BadPicture when clearing a surface
This bug was introduced in 6f431886 "xcb: Prefer RenderFillRectangles to perform
the deferred clear". The problem is that we are trying to clear a surface before
we created a Picture for it. This resulted in a FillRectangles call with
dst=XCB_NONE.

I noticed this problem via cairo-perf-micro's dragon-solid-circle-clip.

Signed-off-by: Uli Schlachter <psychon@znc.in>

src/cairo-xcb-surface-render.c
@@ -2292,6 +2292,7 @@ _cairo_xcb_surface_clear (cairo_xcb_surface_t *dst)
     if (dst->flags & CAIRO_XCB_RENDER_HAS_COMPOSITE) {
 	xcb_render_color_t transparent = { 0 };
 
+	_cairo_xcb_surface_ensure_picture (dst);
 	_cairo_xcb_connection_render_fill_rectangles (dst->connection,
 						      XCB_RENDER_PICT_OP_CLEAR,
 						      dst->picture,