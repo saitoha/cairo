saitoha/cairo

commit 9c0d761bfcdd28d52c83d74f46dd3c709ae0fa69

quartz: improve text transform handling
Use scale instead of manually compositing font_matrix and ctm and
composite it with the context ctm, so that no workaround for clipping
is needed anymore.

src/cairo-quartz-surface.c
@@ -680,8 +680,9 @@ _cairo_quartz_fixup_unbounded_operation (cairo_quartz_surface_t *surface,
     } else if (op->op == UNBOUNDED_SHOW_GLYPHS) {
 	CGContextSetFont (cgc, op->u.show_glyphs.font);
 	CGContextSetFontSize (cgc, 1.0);
-	CGContextSetTextMatrix (cgc, op->u.show_glyphs.textTransform);
+	CGContextSetTextMatrix (cgc, CGAffineTransformIdentity);
 	CGContextTranslateCTM (cgc, op->u.show_glyphs.origin.x, op->u.show_glyphs.origin.y);
+	CGContextConcatCTM (cgc, op->u.show_glyphs.textTransform);
 
 	if (op->u.show_glyphs.isClipping) {
 	    /* Note that the comment in show_glyphs about kCGTextClip
@@ -2482,7 +2483,7 @@ _cairo_quartz_surface_show_glyphs_cg (void *abstract_surface,
 				      cairo_clip_t *clip,
 				      int *remaining_glyphs)
 {
-    CGAffineTransform textTransform, ctm;
+    CGAffineTransform textTransform, ctm, invTextTransform;
 #define STATIC_BUF_SIZE 64
     CGGlyph glyphs_static[STATIC_BUF_SIZE];
     CGSize cg_advances_static[STATIC_BUF_SIZE];
@@ -2581,20 +2582,14 @@ _cairo_quartz_surface_show_glyphs_cg (void *abstract_surface,
 	}
     }
 
-    textTransform = CGAffineTransformMake (scaled_font->font_matrix.xx,
-					   scaled_font->font_matrix.yx,
-					   scaled_font->font_matrix.xy,
-					   scaled_font->font_matrix.yy,
-					   0., 0.);
-    textTransform = CGAffineTransformScale (textTransform, 1.0, -1.0);
-    textTransform = CGAffineTransformConcat (CGAffineTransformMake(scaled_font->ctm.xx,
-								   -scaled_font->ctm.yx,
-								   -scaled_font->ctm.xy,
-								   scaled_font->ctm.yy,
-								   0., 0.),
-					     textTransform);
+    textTransform = CGAffineTransformMake (scaled_font->scale.xx,
+					   scaled_font->scale.yx,
+					   -scaled_font->scale.xy,
+					   -scaled_font->scale.yy,
+					   0, 0);
+    _cairo_quartz_cairo_matrix_to_quartz (&scaled_font->scale_inverse, &invTextTransform);
 
-    CGContextSetTextMatrix (surface->cgContext, textTransform);
+    CGContextSetTextMatrix (surface->cgContext, CGAffineTransformIdentity);
 
     /* Convert our glyph positions to glyph advances.  We need n-1 advances,
      * since the advance at index 0 is applied after glyph 0. */
@@ -2607,32 +2602,15 @@ _cairo_quartz_surface_show_glyphs_cg (void *abstract_surface,
 	cairo_quartz_float_t xf = glyphs[i].x;
 	cairo_quartz_float_t yf = glyphs[i].y;
 	cg_glyphs[i] = glyphs[i].index;
-	cg_advances[i-1].width = xf - xprev;
-	cg_advances[i-1].height = yf - yprev;
+	cg_advances[i - 1] = CGSizeApplyAffineTransform(CGSizeMake (xf - xprev, yf - yprev), invTextTransform);
 	xprev = xf;
 	yprev = yf;
     }
 
-    if (_cairo_quartz_osx_version >= 0x1050 && isClipping) {
-	/* If we're clipping, OSX 10.5 (at least as of 10.5.2) has a
-	 * bug (apple bug ID #5834794) where the glyph
-	 * advances/positions are not transformed by the text matrix
-	 * if kCGTextClip is being used.  So, we pre-transform here.
-	 * 10.4 does not have this problem (as of 10.4.11).
-	 */
-	for (i = 0; i < num_glyphs - 1; i++)
-	    cg_advances[i] = CGSizeApplyAffineTransform(cg_advances[i], textTransform);
-    }
-
-#if 0
-    for (i = 0; i < num_glyphs; i++) {
-	ND((stderr, "[%d: %d %f,%f]\n", i, cg_glyphs[i], cg_advances[i].width, cg_advances[i].height));
-    }
-#endif
-
     /* Translate to the first glyph's position before drawing */
     ctm = CGContextGetCTM (surface->cgContext);
     CGContextTranslateCTM (surface->cgContext, glyphs[0].x, glyphs[0].y);
+    CGContextConcatCTM (surface->cgContext, textTransform);
 
     CGContextShowGlyphsWithAdvances (surface->cgContext,
 				     cg_glyphs,

commit c2b1cba25a91d3b44e39941276212dd6fc91f9cc

test: Add show-glyphs-advance
This new test (based on show-glyphs-many) checks that the glyphs
advances are respected along both axes.

9c0d761bfcdd28d52c83d74f46dd3c709ae0fa69 introduced a bug which
regresses this test in quartz.

Thanks to Jeff Muizelaar for the report!

test/Makefile.am
@@ -1197,6 +1197,11 @@ REFERENCE_IMAGES = \
 	self-intersecting.xlib.ref.png \
 	set-source.ref.png \
 	set-source.rgb24.ref.png \
+	show-glyphs-advance.image16.ref.png \
+	show-glyphs-advance.ps.ref.png \
+	show-glyphs-advance.quartz.ref.png \
+	show-glyphs-advance.ref.png \
+	show-glyphs-advance.svg.ref.png \
 	show-glyphs-many.ref.png \
 	show-text-current-point.image16.ref.png \
 	show-text-current-point.ps2.ref.png \

test/Makefile.sources
@@ -241,6 +241,7 @@ test_sources = \
 	self-copy-overlap.c				\
 	self-intersecting.c				\
 	set-source.c					\
+	show-glyphs-advance.c				\
 	show-glyphs-many.c				\
 	show-text-current-point.c			\
 	skew-extreme.c					\

test/show-glyphs-advance.c
@@ -1,0 +1,107 @@
+/*
+ * Copyright © 2006 Red Hat, Inc.
+ * Copyright © 2011 Andrea Canciani
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * Author: Carl D. Worth <cworth@cworth.org>
+ *         Andrea Canciani <ranma42@gmail.com>
+ */
+
+#include "cairo-test.h"
+
+static cairo_test_status_t
+get_glyph (const cairo_test_context_t *ctx,
+	   cairo_scaled_font_t *scaled_font,
+	   const char *utf8,
+	   cairo_glyph_t *glyph)
+{
+    cairo_glyph_t *text_to_glyphs;
+    cairo_status_t status;
+    int i;
+
+    text_to_glyphs = glyph;
+    i = 1;
+    status = cairo_scaled_font_text_to_glyphs (scaled_font,
+					       0, 0,
+					       utf8, -1,
+					       &text_to_glyphs, &i,
+					       NULL, NULL,
+					       0);
+    if (status != CAIRO_STATUS_SUCCESS)
+	return cairo_test_status_from_status (ctx, status);
+
+    if (text_to_glyphs != glyph) {
+	*glyph = text_to_glyphs[0];
+	cairo_glyph_free (text_to_glyphs);
+    }
+
+    return CAIRO_TEST_SUCCESS;
+}
+
+static const char *characters[] = { "A", "B", "C", "D" };
+
+#define NUM_CHARS ARRAY_LENGTH (characters)
+
+static cairo_test_status_t
+draw (cairo_t *cr, int width, int height)
+{
+    const cairo_test_context_t *ctx = cairo_test_get_context (cr);
+    cairo_scaled_font_t *scaled_font;
+    cairo_glyph_t *glyphs = xmalloc (NUM_CHARS  * sizeof (cairo_glyph_t));
+    int i;
+    cairo_status_t status;
+
+    /* Paint white background. */
+    cairo_set_source_rgb (cr, 1, 1, 1);
+    cairo_paint (cr);
+
+    scaled_font = cairo_get_scaled_font (cr);
+
+    for (i = 0; i < NUM_CHARS; i++) {
+	status = get_glyph (ctx, scaled_font, characters[i], &glyphs[i]);
+	if (status)
+	    goto BAIL;
+
+	glyphs[i].x = 10.0 + 10.0 * (i % 2);
+	glyphs[i].y = 20.0 + 10.0 * (i / 2);
+    }
+
+    cairo_set_source_rgb (cr, 0, 0, 0);
+    cairo_show_glyphs (cr, glyphs, 4);
+
+    cairo_translate (cr, 40., 20.);
+    cairo_rotate (cr, M_PI / 4.);
+
+    cairo_show_glyphs (cr, glyphs, NUM_CHARS);
+
+  BAIL:
+    free(glyphs);
+
+    return status;
+}
+
+CAIRO_TEST (show_glyphs_advance,
+	    "Test that glyph advances work as expected along both axes",
+	    "text, matrix", /* keywords */
+	    NULL, /* requirements */
+	    64, 64,
+	    NULL, draw)