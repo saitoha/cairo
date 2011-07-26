saitoha/cairo

commit a69335a84eb9225b477cc8c753470eb3805b852c

API: map-to-image and create-similar-image
A common requirement is the fast upload of pixel data. In order to
allocate the most appropriate image buffer, we need knowledge of the
destination. The most obvious example is that we could use a
shared-memory region for the image to avoid the transfer cost of
uploading the pixels to the X server. Similarly, gl, win32, quartz...

The other side of the equation is that for manual modification of a
remote surface, it would be more efficient if we can create a similar
image to reduce the transfer costs. This strategy is already followed
for the destination fallbacks and this merely exposes the same
capability for the application fallbacks.

Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

src/cairo-win32-surface.c
@@ -549,6 +549,65 @@ _cairo_win32_surface_get_subimage (cairo_win32_surface_t  *surface,
     return CAIRO_STATUS_SUCCESS;
 }
 
+static cairo_surface_t *
+_cairo_win32_surface_map_to_image (void                    *abstract_surface,
+				   const cairo_rectangle_int_t   *extents)
+{
+    cairo_win32_surface_t *surface = abstract_surface;
+    cairo_win32_surface_t *local = NULL;
+    cairo_status_t status;
+
+    if (surface->image) {
+	GdiFlush();
+	return _cairo_surface_create_for_rectangle_int (surface->image,
+							extents);
+    }
+
+    status = _cairo_win32_surface_get_subimage (abstract_surface,
+						extents->x,
+						extents->y,
+						extents->width,
+						extents->height,
+						&local);
+    if (unlikely (status))
+	return _cairo_surface_create_in_error (status);
+
+    status = _cairo_surface_set_user_data (&local->image->user_data,
+					   (const cairo_user_data_key_t *)surface->image,
+					   local, NULL);
+    if (unlikely (status)) {
+	cairo_surface_destroy (&local->base);
+	return _cairo_surface_create_in_error (status);
+    }
+
+    cairo_surface_set_device_offset (local->image, -extents->x, -extents->y);
+    return local->image;
+}
+
+static cairo_int_status_t
+_cairo_win32_surface_release_unmap_image (void                    *abstract_surface,
+					  cairo_image_surface_t   *image)
+{
+    cairo_win32_surface_t *surface = abstract_surface;
+    cairo_win32_surface_t *local;
+
+    local = _cairo_surface_set_user_data (&image->base.user_data,
+					   (const cairo_user_data_key_t *)surface->image);
+    if (!local)
+	return CAIRO_INT_STATUS_SUCCESS;
+
+    if (!BitBlt (surface->dc,
+		 image->device_transform.x0,
+		 image->device_transform.y0,
+		 image->width, image->height,
+		 local->dc,
+		 0, 0,
+		 SRCCOPY))
+	_cairo_win32_print_gdi_error ("_cairo_win32_surface_release_dest_image");
+
+    return CAIRO_STATUS_SUCCESS;
+}
+
 static cairo_status_t
 _cairo_win32_surface_acquire_source_image (void                    *abstract_surface,
 					   cairo_image_surface_t  **image_out,
@@ -2087,10 +2146,15 @@ _cairo_win32_surface_create_span_renderer (cairo_operator_t	 op,
 
 static const cairo_surface_backend_t cairo_win32_surface_backend = {
     CAIRO_SURFACE_TYPE_WIN32,
+    _cairo_win32_surface_finish,
+
     _cairo_default_context_create,
 
     _cairo_win32_surface_create_similar,
-    _cairo_win32_surface_finish,
+    NULL,
+    _cairo_win32_surface_map_to_image,
+    _cairo_win32_surface_unmap_image,
+
     _cairo_win32_surface_acquire_source_image,
     _cairo_win32_surface_release_source_image,
     _cairo_win32_surface_acquire_dest_image,

src/cairo-analysis-surface.c
@@ -643,10 +643,14 @@ _cairo_analysis_surface_show_text_glyphs (void			    *abstract_surface,
 
 static const cairo_surface_backend_t cairo_analysis_surface_backend = {
     CAIRO_INTERNAL_SURFACE_TYPE_ANALYSIS,
+
+    _cairo_analysis_surface_finish,
     NULL,
 
     NULL, /* create_similar */
-    _cairo_analysis_surface_finish,
+    NULL, /* create_similar_image */
+    NULL, /* map_to_image */
+    NULL, /* unmap */
     NULL, /* acquire_source_image */
     NULL, /* release_source_image */
     NULL, /* acquire_dest_image */
@@ -842,10 +846,14 @@ typedef cairo_int_status_t
 
 static const cairo_surface_backend_t cairo_null_surface_backend = {
     CAIRO_INTERNAL_SURFACE_TYPE_NULL,
+    NULL, /* finish */
+
     _cairo_default_context_create, /* XXX */
 
     NULL, /* create_similar */
-    NULL, /* finish */
+    NULL, /* create similar image */
+    NULL, /* map to image */
+    NULL, /* unmap image*/
     NULL, /* acquire_source_image */
     NULL, /* release_source_image */
     NULL, /* acquire_dest_image */

src/cairo-directfb-surface.c
@@ -1829,10 +1829,15 @@ _cairo_directfb_surface_is_similar (void *surface_a, void *surface_b)
 static cairo_surface_backend_t
 _cairo_directfb_surface_backend = {
          CAIRO_SURFACE_TYPE_DIRECTFB, /*type*/
+        _cairo_directfb_surface_finish, /*finish*/
+
 	 _cairo_default_context_create,
 
         _cairo_directfb_surface_create_similar,/*create_similar*/
-        _cairo_directfb_surface_finish, /*finish*/
+	NULL, /* create similar image */
+	NULL, /* map to image */
+	NULL, /* unmap image */
+
         _cairo_directfb_surface_acquire_source_image,/*acquire_source_image*/
         _cairo_directfb_surface_release_source_image,/*release_source_image*/
         _cairo_directfb_surface_acquire_dest_image,/*acquire_dest_image*/

src/cairo-error-private.h
@@ -41,6 +41,8 @@
 #include "cairo.h"
 #include "cairo-compiler-private.h"
 
+#include <assert.h>
+
 CAIRO_BEGIN_DECLS
 
 enum _cairo_int_status {
@@ -99,6 +101,13 @@ enum _cairo_int_status {
 
 #define _cairo_int_status_is_error(status) \
     (status != CAIRO_INT_STATUS_SUCCESS && status <= CAIRO_INT_STATUS_LAST_STATUS)
+
+static inline cairo_status_t
+_cairo_public_status (cairo_int_status_t status)
+{
+    assert (status <= CAIRO_INT_STATUS_LAST_STATUS);
+    return status;
+}
 
 cairo_private cairo_status_t
 _cairo_error (cairo_status_t status);

src/cairo-gl-surface.c
@@ -1764,10 +1764,13 @@ _cairo_gl_surface_fill (void			*abstract_surface,
 
 const cairo_surface_backend_t _cairo_gl_surface_backend = {
     CAIRO_SURFACE_TYPE_GL,
+    _cairo_gl_surface_finish,
     _cairo_default_context_create,
 
     _cairo_gl_surface_create_similar,
-    _cairo_gl_surface_finish,
+    NULL, /* similar image */
+    NULL, /* map to image */
+    NULL, /* unmap image */
 
     _cairo_gl_surface_acquire_source_image,
     _cairo_gl_surface_release_source_image,

src/cairo-image-surface.c
@@ -728,6 +728,36 @@ _cairo_image_surface_create_similar (void	       *abstract_other,
 						     width, height);
 }
 
+static cairo_surface_t *
+_cairo_image_surface_map_to_image (void *abstract_other,
+				   const cairo_rectangle_int_t *extents)
+{
+    cairo_image_surface_t *other = abstract_other;
+    cairo_surface_t *surface;
+    uint8_t *data;
+
+    data = other->data;
+    data += extents->y * other->stride;
+    data += extents->x * PIXMAN_FORMAT_BPP (other->pixman_format)/ 8;
+
+    surface =
+	_cairo_image_surface_create_with_pixman_format (data,
+							other->pixman_format,
+							extents->width,
+							extents->height,
+							other->stride);
+
+    cairo_surface_set_device_offset (surface, -extents->x, -extents->y);
+    return surface;
+}
+
+static cairo_int_status_t
+_cairo_image_surface_unmap_image (void *abstract_surface,
+				  cairo_image_surface_t *image)
+{
+    return CAIRO_INT_STATUS_SUCCESS;
+}
+
 static cairo_status_t
 _cairo_image_surface_finish (void *abstract_surface)
 {
@@ -4655,10 +4685,15 @@ _cairo_surface_is_image (const cairo_surface_t *surface)
 
 const cairo_surface_backend_t _cairo_image_surface_backend = {
     CAIRO_SURFACE_TYPE_IMAGE,
+    _cairo_image_surface_finish,
+
     _cairo_default_context_create,
 
     _cairo_image_surface_create_similar,
-    _cairo_image_surface_finish,
+    NULL, /* create similar image */
+    _cairo_image_surface_map_to_image,
+    _cairo_image_surface_unmap_image,
+
     _cairo_image_surface_acquire_source_image,
     _cairo_image_surface_release_source_image,
     _cairo_image_surface_acquire_dest_image,

src/cairo-os2-surface.c
@@ -1439,10 +1439,14 @@ _cairo_os2_surface_mark_dirty_rectangle (void *surface,
 
 static const cairo_surface_backend_t cairo_os2_surface_backend = {
     CAIRO_SURFACE_TYPE_OS2,
+    _cairo_os2_surface_finish,
     _cairo_default_context_create,
 
     NULL, /* create_similar */
-    _cairo_os2_surface_finish,
+    NULL, /* create_similar_image */
+    NULL, /* map_to_image */
+    NULL, /* unmap_image */
+
     _cairo_os2_surface_acquire_source_image,
     _cairo_os2_surface_release_source_image,
     _cairo_os2_surface_acquire_dest_image,

src/cairo-paginated-surface.c
@@ -649,10 +649,15 @@ _cairo_paginated_context_create (void *target)
 
 static const cairo_surface_backend_t cairo_paginated_surface_backend = {
     CAIRO_INTERNAL_SURFACE_TYPE_PAGINATED,
+    _cairo_paginated_surface_finish,
+
     _cairo_paginated_context_create,
 
     _cairo_paginated_surface_create_similar,
-    _cairo_paginated_surface_finish,
+    NULL, /* create simlar image */
+    NULL, /* map to image */
+    NULL, /* unmap image */
+
     _cairo_paginated_surface_acquire_source_image,
     _cairo_paginated_surface_release_source_image,
     NULL, /* acquire_dest_image */

commit 848066691c1fe8559a48a954a1def0fdc3427d88

win32: Fix compilation
a69335a84eb9225b477cc8c753470eb3805b852c broke compilaiton of win32
surfaces because of some typos.

src/cairo-win32-surface.c
@@ -572,9 +572,9 @@ _cairo_win32_surface_map_to_image (void                    *abstract_surface,
     if (unlikely (status))
 	return _cairo_surface_create_in_error (status);
 
-    status = _cairo_surface_set_user_data (&local->image->user_data,
-					   (const cairo_user_data_key_t *)surface->image,
-					   local, NULL);
+    status = cairo_surface_set_user_data (local->image,
+					  (const cairo_user_data_key_t *)surface->image,
+					  local, NULL);
     if (unlikely (status)) {
 	cairo_surface_destroy (&local->base);
 	return _cairo_surface_create_in_error (status);
@@ -585,20 +585,20 @@ _cairo_win32_surface_map_to_image (void                    *abstract_surface,
 }
 
 static cairo_int_status_t
-_cairo_win32_surface_release_unmap_image (void                    *abstract_surface,
-					  cairo_image_surface_t   *image)
+_cairo_win32_surface_unmap_image (void                    *abstract_surface,
+				  cairo_image_surface_t   *image)
 {
     cairo_win32_surface_t *surface = abstract_surface;
     cairo_win32_surface_t *local;
 
-    local = _cairo_surface_set_user_data (&image->base.user_data,
-					   (const cairo_user_data_key_t *)surface->image);
+    local = cairo_surface_get_user_data (&image->base,
+					 (const cairo_user_data_key_t *) surface->image);
     if (!local)
 	return CAIRO_INT_STATUS_SUCCESS;
 
     if (!BitBlt (surface->dc,
-		 image->device_transform.x0,
-		 image->device_transform.y0,
+		 image->base.device_transform.x0,
+		 image->base.device_transform.y0,
 		 image->width, image->height,
 		 local->dc,
 		 0, 0,