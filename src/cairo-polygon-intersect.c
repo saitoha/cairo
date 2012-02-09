saitoha/cairo

commit 658fa75a5c78c8ca08bc5c1f8f29d50a9da0aaf5

polygon: Extend intersection edges to cover entire range
By simply swapping the continuation edges, we end up with a set of edges
that are defined over a shorter range than their extents. Whilst this is
numerically stable at our normal precision we start to encounter issues
when using a coarser grid during rasterisation as the derivative of the
edge becomes unstable.

Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=44722
Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

src/cairo-polygon-intersect.c
@@ -1179,8 +1179,17 @@ edges_start_or_continue (cairo_bo_edge_t	*left,
 
     if (left->deferred.other != NULL) {
 	if (right != NULL && edges_colinear (left->deferred.other, right)) {
-	    /* continuation on right, so just swap edges */
-	    assert (left->deferred.other->deferred.other == NULL);
+	    cairo_bo_edge_t *old = left->deferred.other;
+
+	    /* continuation on right, extend right to cover both */
+	    assert (old->deferred.other == NULL);
+	    assert (old->edge.dir == right->edge.dir);
+	    assert (old->edge.line.p2.y > old->edge.line.p1.y);
+
+	    if (old->edge.line.p1.y < right->edge.line.p1.y)
+		right->edge.line.p1 = old->edge.line.p1;
+	    if (old->edge.line.p2.y > right->edge.line.p2.y)
+		right->edge.line.p2 = old->edge.line.p2;
 	    left->deferred.other = right;
 	    return;
 	}

commit 1370b84279f59c35570fa6338acd9e3ba12e76fe

polygon-intersection: The edge direction is immaterial
When checking for continuations on the right-hand edge, the actual
direction of the edge is immaterial as it is sorted into ascending
y-order and the direction is fixed up when emitting into the output
polygon.

Fixes assertion introduced with 658fa75a5c78c8.

Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

src/cairo-polygon-intersect.c
@@ -1183,7 +1183,6 @@ edges_start_or_continue (cairo_bo_edge_t	*left,
 
 	    /* continuation on right, extend right to cover both */
 	    assert (old->deferred.other == NULL);
-	    assert (old->edge.dir == right->edge.dir);
 	    assert (old->edge.line.p2.y > old->edge.line.p1.y);
 
 	    if (old->edge.line.p1.y < right->edge.line.p1.y)