saitoha/cairo

commit 2283ab969801ef6c610ad20a58693a5b55382c8a

xcb: Handle SHM exhaustion gracefully
Avoid the assertion failure of creating an error surface for an internal
status by handling the expected UNSUPPORTED condition.

Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

src/cairo-xcb-surface.c
@@ -240,8 +240,12 @@ _cairo_xcb_surface_create_shm_image (cairo_xcb_connection_t *connection,
 						      stride * height,
 						      might_reuse,
 						      &shm_info);
-    if (unlikely (status))
+    if (unlikely (status)) {
+	if (status == CAIRO_INT_STATUS_UNUSPPORTED)
+	    return NULL;
+
 	return _cairo_surface_create_in_error (status);
+    }
 
     image = _cairo_image_surface_create_with_pixman_format (shm_info->mem,
 							    pixman_format,
@@ -354,9 +358,7 @@ _get_image (cairo_xcb_surface_t		 *surface,
     if (use_shm) {
 	image = _get_shm_image (surface, x, y, width, height);
 	if (image) {
-	    /* XXX This only wants to catch SHM exhaustion,
-	     * not other allocation failures. */
-	    if (image->status != CAIRO_STATUS_NO_MEMORY) {
+	    if (image->status) {
 		_cairo_xcb_connection_release (connection);
 		return image;
 	    }

commit 1abe989eb56bf37601d3a6164990fc88ab82d802

xcb: Fix SHM in _get_image()
Commit 2283ab9 introduced a logic error. Instead of falling back to the non-SHM
path when getting the image via SHM failed, we now did the fallback when getting
the image via SHM worked (which means that the SHM operation was a waste of
time).

Signed-off-by: Uli Schlachter <psychon@znc.in>

src/cairo-xcb-surface.c
@@ -367,7 +367,7 @@ _get_image (cairo_xcb_surface_t		 *surface,
     if (use_shm) {
 	image = _get_shm_image (surface, x, y, width, height);
 	if (image) {
-	    if (image->status) {
+	    if (image->status == CAIRO_STATUS_SUCCESS) {
 		_cairo_xcb_connection_release (connection);
 		return image;
 	    }